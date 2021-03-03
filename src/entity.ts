2dc5d7f97b80a490fd758b07512f2102e69d1d61
Staś Małolepszy <stas@piesku.com>
2021-03-03 21:57:27 +0100

    World.CreateEntity, World.DestroyEntity

--- src/entity.ts
+++ src/entity.ts
@@ -3,35 +3,6 @@ import {transform2d} from "./components/com_transform2d.js";
 import {Entity, Game} from "./game.js";
 import {Has, World} from "./world.js";
 
-export function create_entity(world: World) {
-    if (world.Graveyard.length > 0) {
-        return world.Graveyard.pop()!;
-    }
-
-    if (DEBUG && world.Signature.length > 10000) {
-        throw new Error("No more entities available.");
-    }
-
-    // Push a new signature and return its index.
-    return world.Signature.push(0) - 1;
-}
-
-export function destroy_entity(world: World, entity: Entity) {
-    if (world.Signature[entity] & Has.Transform2D) {
-        for (let child of world.Transform2D[entity].Children) {
-            destroy_entity(world, child);
-        }
-    }
-
-    world.Signature[entity] = 0;
-
-    if (DEBUG && world.Graveyard.includes(entity)) {
-        throw new Error("Entity already in graveyard.");
-    }
-
-    world.Graveyard.push(entity);
-}
-
 type Mixin = (game: Game, entity: Entity) => void;
 export interface Blueprint2D {
     Translation?: Vec2;
@@ -45,7 +16,7 @@ export function instantiate(
     game: Game,
     {Translation, Rotation, Scale, Using = [], Children = []}: Blueprint2D
 ) {
-    let entity = create_entity(game.World);
+    let entity = game.World.CreateEntity();
     transform2d(Translation, Rotation, Scale)(game, entity);
     for (let mixin of Using) {
         mixin(game, entity);
@@ -58,4 +29,13 @@ export function instantiate(
         entity_transform.Children.push(child);
     }
     return entity;
+}
+
+export function destroy(world: World, entity: Entity) {
+    if (world.Signature[entity] & Has.Transform2D) {
+        for (let child of world.Transform2D[entity].Children) {
+            destroy(world, child);
+        }
+    }
+    world.DestroyEntity(entity);
 }
